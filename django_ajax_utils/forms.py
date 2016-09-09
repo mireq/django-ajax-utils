58311ee9e63d3d3a5deb9a8d00baab75d91c360e
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-09-09 15:47:21 +0200

    Added SetRequiredFieldsMixin

--- django_ajax_utils/forms.py
+++ django_ajax_utils/forms.py
@@ -17,3 +17,24 @@ class AutoPlaceholderFormMixin(object):
 				placeholder = field.help_text or field.label
 				if not isinstance(field.label, SafeText) and not isinstance(placeholder, CheckboxInput):
 					field.widget.attrs['placeholder'] = placeholder
+
+
+class SetRequiredFieldsMixin(object):
+	"""
+	Overrides required attribute for selected fields.
+
+	Example
+
+	class Foo(SetRequiredFieldsMixin, forms.ModelForm):
+		required_fields = {
+			'field1': True,
+			'field2': False,
+		}
+	"""
+
+	required_fields = {}
+
+	def __init__(self, *args, **kwargs):
+		super(SetRequiredFieldsMixin, self).__init__(*args, **kwargs)
+		for fieldname, is_required in self.required_fields.iteritems():
+			self.fields[fieldname].required = is_required
