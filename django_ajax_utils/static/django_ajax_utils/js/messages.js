6328caaccf13009b3b79ba1b6cdd927850cceeb0
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-10-16 13:53:50 +0200

    Added messageShow method

--- django_ajax_utils/static/django_ajax_utils/js/messages.js
+++ django_ajax_utils/static/django_ajax_utils/js/messages.js
@@ -6,6 +6,8 @@ var message = function(element, options) {
 	opts = _.lightCopy(opts);
 	opts.autoclose = opts.autoclose || _.getData(element, 'autoclose');
 
+	self.element = element;
+
 	self.close = function() {
 		if (opts.animateClose) {
 			_.addClass(element, 'closing');
@@ -41,12 +43,17 @@ var message = function(element, options) {
 	return self;
 };
 
-var registerMessages = function(element) {
+var getOptsForElement = function(element) {
 	var opts = {};
 	var animateClose = parseFloat(_.getData(element, 'animateClose'), 10);
 	if (!isNaN(animateClose)) {
 		opts.animateClose = Math.round(animateClose * 1000);
 	}
+	return opts;
+};
+
+var registerMessages = function(element) {
+	var opts = getOptsForElement(element);
 	_.forEach(_.cls(element, 'message'), function(element) {
 		message(element, opts);
 	});
@@ -58,6 +65,54 @@ var register = function(element) {
 		registerMessages(element);
 	});
 };
+
+
+var messageConstruct = function(options) {
+	var msg = _.elem('DIV', {'class': 'message'});
+	var msgBox = _.elem('DIV', {'class': 'message-box ' + options.cls});
+	var msgContent = _.elem('DIV', {'class': 'message-content ' + options.cls});
+	if (options.messageText !== undefined) {
+		var txt = document.createTextNode(options.messageText);
+		msgContent.appendChild(txt);
+	}
+	else {
+		msgContent.innerHTML = options.messageHTML;
+	}
+	if (options.includeClose) {
+		var closeAction = _.elem('A', {'href': '#', 'aria-hidden': 'true', 'class': 'close-action'}, '⨉');
+		msgContent.appendChild(closeAction);
+	}
+	msgBox.appendChild(msgContent);
+	msg.appendChild(msgBox);
+	return msg;
+};
+
+
+var messageShow = function(options) {
+	var opts = options || {};
+	opts = _.lightCopy(opts);
+	opts.messageText = opts.messageText || null;
+	opts.messageHTML = opts.messageHTML || null;
+	opts.includeClose = opts.includeClose || true;
+	opts.container = opts.container || _.id('messages_container');
+	opts.cls = opts.cls || '';
+
+	var elementOpts = getOptsForElement(opts.container);
+	for (var key in elementOpts) {
+		if (_.has(elementOpts, key)) {
+			if (opts[key] === undefined) {
+				opts[key] = elementOpts[key];
+			}
+		}
+	}
+
+	var messageElement = messageConstruct(opts);
+	opts.container.appendChild(messageElement);
+	return message(messageElement, opts);
+};
+
+
+_.messageShow = messageShow;
 
 _.onLoad(function(e) { register(e.memo); });
 
