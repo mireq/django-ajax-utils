ffac2ca246c9fad1698aa61f8a2c4deb01bc878e
Miroslav Bendík <miroslav.bendik@gmail.com>
2017-04-24 09:51:23 +0200

    Added isArray

--- django_ajax_utils/static/django_ajax_utils/js/utils.js
+++ django_ajax_utils/static/django_ajax_utils/js/utils.js
@@ -107,6 +107,8 @@ else {
 	};
 }
 
+
+
 var map;
 if (Array.prototype.map) {
 	var coreMap = Array.prototype.map;
@@ -829,6 +831,12 @@ window._utils.encodeURLParameters = getUrlParameterByName;
 
 if (!Function.prototype.bind) {
 	Function.prototype.bind=function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}function c(){}var a=[].slice,f=a.call(arguments,1),e=this,d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;}; // jshint ignore:line
+}
+
+if (typeof Array.isArray !== 'function') {
+	Array.isArray = function(arr) {
+		return Object.prototype.toString.call(arr) === '[object Array]';
+	};
 }
 
 }());
